7b48455c165779851ec5d31bc2c348e45154571b
allenjherusantiago <allenjherussantiago@gmail.com>
2024-09-30 23:21:29 +0800

    Update settings.py
    Turned off Debug mode

--- Week_1_Intro_To_MVC/weatherProject/weatherProject/settings.py
+++ Week_1_Intro_To_MVC/weatherProject/weatherProject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d0(_451-s&4c=a$gnq^uecxw(+c-xd%$ei6&uqgwo-9xno@gwf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
